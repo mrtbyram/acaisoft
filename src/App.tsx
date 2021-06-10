6245f7e811224bac95f28a3a1ce12ad2060dc2b0
Murat Bayram <m.byram@gmail.com>
2021-06-10 23:06:30 +0300

    template initialize

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ import React from 'react';
 function App() {
 
   return (
-    <div className='bg'>
-    <p>.</p>
+    <>
+    <div id="preloader">
+      <div className="jumper">
+        <div />
+        <div />
+        <div />
+      </div>
     </div>
+    </>
   );
 }
 
