66821657312f927cf82ed9ebfb10aca33e04f5a4
Murat Bayram <m.byram@gmail.com>
2021-06-10 23:36:50 +0300

    working pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import './App.css';
 import React from 'react';
+import Header from './components/Header';
+import Welcome from './components/Welcome';
 
 function App() {
 
   return (
     <>
-    <div id="preloader">
+    {/* <div id="preloader">
       <div className="jumper">
         <div />
         <div />
         <div />
       </div>
-    </div>
+    </div> */}
+    <Header />
+    <Welcome />
     </>
   );
 }
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Welcome = () => {
+    return(
+        <>
+        <div className="welcome-area" id="welcome">
+        <div className="header-text">
+          <div className="container">
+            <div className="row">
+              <div className="offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 col-md-12 col-sm-12">
+                <h1>We provide the best <strong>strategy</strong><br />to grow up your <strong>business</strong></h1>
+                <p>Softy Pinko is a professional Bootstrap 4.0 theme designed by Template Mo 
+                  for your company at absolutely free of charge</p>
+                <a href="#features" className="main-button-slider">Discover More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        </>
+    )
+}
+
+export default Welcome;
