8b1c7243274ab3e3cd7148e1fe196dd03867e783
Murat Bayram <m.byram@gmail.com>
2021-06-10 22:58:43 +0300

    imports

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import 'bootstrap';
+import 'jquery';
 
 ReactDOM.render(
   <React.StrictMode>
